af4d5beadcbac8efd83a13bde75b94c58f951c2e
Kadir <abdulkadirmohdaris@gmail.com>
2020-03-02 23:18:48 +0800

    index profile dummy data

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="row">
+	<div class="col-10 offset-1">
+		<table class="table table-striped table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Picture</th>
+					<th>Name</th>
+					<th>Nationality</th>
+					<th>Position</th>
+					<th>Current Team</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+				</tr>
+				<tr>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+				</tr>
+				<tr>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+					<td>asdas</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+
+@endsection
